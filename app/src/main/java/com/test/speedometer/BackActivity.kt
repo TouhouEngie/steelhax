70728527e1a5da82b13d7cc201619cd7d2f86c93
George W. <00_sling_shears@icloud.com>
2026-09-19 14:12:27 -0400

    refactor 2

--- app/src/main/java/com/test/speedometer/BackActivity.kt
+++ app/src/main/java/com/test/speedometer/BackActivity.kt
@@ -1,5 +1,6 @@
 package com.test.speedometer
 
+import android.hardware.SensorManager
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
@@ -9,14 +10,19 @@ class BackActivity {
 
 @Composable
 fun DetermineSpeed(rate: Int) {
+
     // x side by side, y up and down, z through...
     // we're gonna need to get rotational data to determine how much of the z and y axes we'll need to use
+
+    // take sensor data every 100 milliseconds
     for (i in 1..rate) {
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val accelerationSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
         val rotationSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+
+
+        Thread.sleep(100);
     }
-    Text(
-        text = "Acceleration Data: $accelerationSensor, Rotational Data: $rotationSensor"
-    )
+
 }
+
